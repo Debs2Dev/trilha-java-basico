81ea17a9f723b045a1a884b2b7b7ea56f47b9c8d
Debs2Dev <deboramob17@gmail.com>
2024-07-21 22:42:21 -0300

    segundo commit

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("O pull e push não funciona!");
+        System.out.println("Funcionou!");
+               
         
     }
 }
